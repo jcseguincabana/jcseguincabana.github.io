1ad13a934a0a86f20472cd01d52eab61a78a27f3
Jeececab <jcseguincabana@gmail.com>
2018-03-17 20:31:15 -0400

    Creates css and script files

---
+++ script.js
@@ -0,0 +1,13 @@
+const colors = ['yellow', 'blue', 'pink', 'green', 'white', 'red', 'orange'];
+
+function stroboscope() {
+	const strobTainer = document.getElementById('stroboscope');
+	let i = 0;
+	setInterval(function() {
+		strobTainer.style.backgroundColor = colors[i];
+		i++;
+		if (i === colors.length - 1) {
+			i = 0;
+		};
+	}, 1000);
+};
