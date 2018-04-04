ee8851fa7bf5570ad45a183a0a8ed7fc60b89cd2
Jean-Christophe Seguin Cabana <jcseguincabana@gmail.com>
2018-04-04 11:46:23 -0400

    Updates everything

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const colors = ['yellow', 'blue', 'pink', 'green', 'white', 'red', 'orange'];
+// const colors = ['yellow', 'blue', 'pink', 'green', 'white', 'red', 'orange'];
+// const stroboscope = document.getElementById('stroboscope');
+// var startStrob;
+// let i = 0;
+// const myInterval = setInterval(function() {
+// 		stroboscope.style.backgroundColor = colors[i];
+// 		i++;
+// 		if (i === colors.length - 1) {
+// 			i = 0;
+// 		};
+// 	}, 50);
 
-function stroboscope() {
-	const strobTainer = document.getElementById('stroboscope');
-	let i = 0;
-	setInterval(function() {
-		strobTainer.style.backgroundColor = colors[i];
-		i++;
-		if (i === colors.length - 1) {
-			i = 0;
-		};
-	}, 50);
-};
+// function startStrob() {	
+// 	startStrob = myInterval;
+// };
+
+// function stopStrob() {
+// 	window.clearInterval(startStrob);
+// };
+
+
+
+// stroboscope.addEventListener('click', function(e) {
+// 	console.log(e.target.className);
+// 	if (e.target.className === 'on') {
+// 		startStrob();
+// 		e.target.className = 'off';
+// 	} else if (e.target.className = 'off') {
+// 		stopStrob();
+// 		e.target.className = 'on';
+// 		startStrob = myInterval;
+// 	}
+// });
