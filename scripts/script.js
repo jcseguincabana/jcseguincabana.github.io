ef3ed1cacc94f4397385f11b83e7bf966ff69e03
Jean-Christophe Seguin Cabana <jcseguincabana@gmail.com>
2018-04-15 12:00:47 -0400

    css and content updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 
-
+// Parallax background
 $(window).scroll(() => {
   const scrollTopValue = $(window).scrollTop();
   $(".parallax-bg").css("background-position", "center " + scrollTopValue*0.6 + "px");
 });
 
-
+// Arrow 
 $(window).scroll(() => {
   const scrollTopValue = $(window).scrollTop();
   if (scrollTopValue > 25) {
@@ -13,4 +13,13 @@ $(window).scroll(() => {
   } else {
     $(".arrow").fadeIn("medium");
   }
+});
+
+// Smooth scrolling for anchors
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 100);
 });
