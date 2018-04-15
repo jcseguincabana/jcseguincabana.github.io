0ca62771b691171018988d7daae76786fbdc0cc5
Jean-Christophe Seguin Cabana <jcseguincabana@gmail.com>
2018-04-15 10:13:50 -0400

    add arrow

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,13 @@ $(window).scroll(() => {
   const scrollTopValue = $(window).scrollTop();
   $(".parallax-bg").css("background-position", "center " + scrollTopValue*0.6 + "px");
 });
+
+
+$(window).scroll(() => {
+  const scrollTopValue = $(window).scrollTop();
+  if (scrollTopValue > 25) {
+    $(".arrow").fadeOut("medium");
+  } else {
+    $(".arrow").fadeIn("medium");
+  }
+});
